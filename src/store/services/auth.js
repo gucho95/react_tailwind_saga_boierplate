4efbfd492a3b94695d458a7b75db14db7a0c948a
matevosyanmko <matevosyanmko@gmail.com>
2020-10-10 13:28:33 +0400

    fixes

--- src/store/services/auth.js
+++ src/store/services/auth.js
@@ -4,5 +4,8 @@ export default {
   facebookLogin: () => new Promise(async (resolve) => FB.login((response) => resolve(response))),
   facebookLogout: () => new Promise(async (resolve) => FB.logout((response) => resolve(response))),
   facebookGetLoginStatus: () => new Promise(async (resolve) => FB.getLoginStatus((response) => resolve(response))),
-  facebookMe: () => new Promise(async (resolve) => FB.api("/me", (response) => resolve(response))),
+  facebookMe: () =>
+    new Promise(async (resolve) =>
+      FB.api("/me", { fields: "birthday,email,hometown" }, (response) => resolve(response))
+    ),
 };
